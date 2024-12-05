f3cc620d2d44d0d2a5c10878b6c8d5cc0de05c17
benjamin-sa <benkee.sauter@gmail.com>
2024-12-05 12:24:34 +0100

    Base64 encoding

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,10 +1,31 @@
 // server/config/firebaseAdmin.js
 const admin = require('firebase-admin');
-const path = require('path');
+const dotenv = require('dotenv');
 
-admin.initializeApp({
-  credential: admin.credential.cert(path.join(__dirname, 'scoutswinkel-firebase-adminsdk-ucote-7f034f7aec.json')),
-  databaseURL: 'https://scoutswinkel.firebaseio.com'
-});
+// Laad environment variables uit .env
+dotenv.config();
+
+// Haal de base64-gecodeerde service account op uit de environment variable
+const base64ServiceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
+
+if (base64ServiceAccount) {
+  // Decodeer de base64-string
+  const decodedServiceAccount = Buffer.from(base64ServiceAccount, 'base64').toString('utf-8');
+  
+  // Parse de JSON-string naar een object
+  const serviceAccount = JSON.parse(decodedServiceAccount);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    // Voeg hier eventuele extra configuratie toe, zoals databaseURL
+  });
+} else {
+  // Fallback voor lokaal testen met JSON-bestand
+  const serviceAccount = require('./scoutswinkel-firebase-adminsdk-ucote-7f034f7aec.json');
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
