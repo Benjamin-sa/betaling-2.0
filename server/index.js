0c1c5420e009a935242c475d282b061d9e50f848
Benjamin Sauter <benkee.sauter@gmail.com>
2025-02-08 17:41:36 +0100

    feat: refactor backup service instantiation and usage in server startup

--- server/index.js
+++ server/index.js
@@ -4,26 +4,24 @@ const cors = require('cors');
 const database = require('./db');
 const Migrations = require('./db/migrations');
 const path = require('path');
-
+const BackupService = require('./services/backup.service'); // Import the class
 
 async function startServer() {
   try {
     // Connect to database
     await database.connect();
 
-    const BackupService = require('./services/backup.service');
-
-
     // Run migrations
     const migrations = new Migrations(database.instance);
     await migrations.runMigrations();
 
-    const backUp = require('./services/backup.service');
+    // Instantiate BackupService
+    const backupService = new BackupService();
 
     // Restore data from latest backup if database is empty
     const isEmpty = await checkIfDatabaseIsEmpty();
     if (isEmpty) {
-      await backUp.restoreFromFirestore();
+      await backupService.restoreFromFirestore();
     }
 
     const app = express();
@@ -37,7 +35,6 @@ async function startServer() {
     app.use(express.json());
 
     app.use(express.static(path.join(__dirname, '../client/dist')));
-
 
     // Routes
     app.use('/api/products', require('./routes/product.routes'));
@@ -54,34 +51,32 @@ async function startServer() {
 
     // Schedule regular backups (every 8 hours)
     setInterval(() => {
-      backUp.backupToFirestore();
+      backupService.backupToFirestore(); // Use the instance
     }, 8 * 60 * 60 * 1000);
-
 
     // Start server
     app.listen(port, () => {
       console.log(`Server running at http://localhost:${port}`);
     });
-    
-    if (process.env.NODE_ENV === 'production') {
-    // Handle shutdown
-    process.on('SIGINT', async () => {
-      try {
-        await BackupService.backupToFirestore();
-        await database.close();
-        process.exit(0);
-      } catch (error) {
-        console.error('Error during shutdown:', error);
-        process.exit(1);
-      }
-    });}
 
+    if (process.env.NODE_ENV === 'production') {
+      // Handle shutdown
+      process.on('SIGINT', async () => {
+        try {
+          await backupService.backupToFirestore(); // Use the instance
+          await database.close();
+          process.exit(0);
+        } catch (error) {
+          console.error('Error during shutdown:', error);
+          process.exit(1);
+        }
+      });
+    }
   } catch (error) {
     console.error('Failed to start server:', error);
     process.exit(1);
   }
 }
-
 
 async function checkIfDatabaseIsEmpty() {
   return new Promise((resolve, reject) => {
